3b65198379793471df47cb7fb14349001f36b8fa
MatUrbanski <mateuszurbanski@yahoo.pl>
2014-12-26 19:12:39 +0100

    Missing associations

--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -2,4 +2,7 @@ class Treatment < ActiveRecord::Base
   belongs_to :patient
   belongs_to :doctor
   belongs_to :treatment_type
+
+  has_many :treatment_times
+  has_many :visitation_times, through: :treatment_times
 end
--- app/models/visitation_time.rb
+++ app/models/visitation_time.rb
@@ -1,3 +1,6 @@
 class VisitationTime < ActiveRecord::Base
+  has_many :treatment_times
+  has_many :treatments, through: :treatment_times
+
   validates :day, :time_of_day, presence: true
 end
